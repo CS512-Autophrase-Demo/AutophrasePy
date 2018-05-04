ce95a25e0878090fe20d45fd0c44e7368db3c73a
Renxuanwang <290484989@qq.com>
2018-05-04 16:33:28 -0500

    rename variables,; add static function to wrap

--- autophrase/autophrase.py
+++ autophrase/autophrase.py
@@ -3,79 +3,78 @@ import shutil
 from .pos_tag import pos_tag
 
 class Autophrase:
-    def __init__(self):
+    def __init__(self, highlight_multi=0.8, highlight_single=1.0, thread=10, pos_tagging=True):
         self.root_path = os.path.dirname(os.path.abspath(__file__))
         self.curent_directory = os.getcwd()
-        self.installation_path = os.path.dirname(os.path.abspath(__file__))
-        self.HIGHLIGHT_MULTI = "0.8"
-        self.HIGHLIGHT_SINGLE = "1.0"
-        self.ENABLE_POS_TAGGING = 1
-        self.THREAD = "10"
+        # self.installation_path = os.path.dirname(os.path.abspath(__file__))
+        self.highlight_multi = str(highlight_multi)
+        self.highlight_single = str(highlight_single)
+        self.enable_pos_tagging = pos_tagging
+        self.thread = str(thread)
         self.model = self.root_path + "/models/DBLP"
-        self.SEGMENTATION_MODEL = self.model + "/segmentation.model"
+        self.segmentation_model = self.model + "/segmentation.model"
         self.tmp = self.curent_directory + "/tmp"
-        #self.LANGUAGE = "UNKNOWN"
         tokenizer_path = self.root_path + ":" + self.root_path + "/tools/tokenizer/lib/*" + ":" + self.root_path + "/tools/tokenizer/resources/" + ":" + self.root_path + "/tools/tokenizer/build/"
-        self.TOKENIZER = "-cp " + tokenizer_path + " Tokenizer"
-        
+        self.tokenizer = "-cp " + tokenizer_path + " Tokenizer"
+    
+    @staticmethod
+    def phrasal_segment():
+        a = Autophrase()
+        a.tokenize("DBLP.5K.txt", "EN")
+        a.pos_tagging()
+        a.phrase_segmentation()
+        a.generate_output()
+
     def tokenize(self, filename, language):
-        self.TEXT_TO_SEG = filename
-        TOKEN_MAPPING = self.model + "/token_mapping.txt"
+        self.text_to_seg = filename
+        token_mapping = self.model + "/token_mapping.txt"
         if os.path.exists(self.tmp):
             shutil.rmtree(self.tmp)
         os.mkdir(self.tmp)
         print("===Tokenization===")
-        TOKENIZED_TEXT_TO_SEG = self.tmp + "/tokenized_text_to_seg.txt"
+        tokenized_text_to_seg = self.tmp + "/tokenized_text_to_seg.txt"
         CASE = self.tmp + "/case_tokenized_text_to_seg.txt"
-        os.system("java " + self.TOKENIZER + " -m direct_test -i " + self.TEXT_TO_SEG + " -o " + TOKENIZED_TEXT_TO_SEG + " -t " + TOKEN_MAPPING + " -c N -thread " + self.THREAD)
+        os.system("java " + self.tokenizer + " -m direct_test -i " + self.text_to_seg + " -o " + tokenized_text_to_seg + " -t " + token_mapping + " -c N -thread " + self.thread)
         #language_file = open(self.tmp + "/language.txt", 'r')
-        self.LANGUAGE = language
-        print("Detected Language: " + self.LANGUAGE)
+        self.language = language
+        print("Detected Language: " + self.language)
 
     def pos_tagging(self):
-        if self.LANGUAGE != "JA"and self.LANGUAGE != "CN" and self.LANGUAGE != "OTHER" and self.ENABLE_POS_TAGGING == 1:
+        if self.language != "JA"and self.language != "CN" and self.language != "OTHER" and self.enable_pos_tagging == 1:
             RAW = self.tmp + "/raw_tokenized_text_to_seg.txt"            
-            pos_tag("EN", int(self.THREAD), RAW, self.tmp)
+            pos_tag("EN", int(self.thread), RAW, self.tmp)
             os.rename(self.tmp + "/pos_tags.txt", self.tmp + "/pos_tags_tokenized_text_to_seg.txt")
 
     def phrase_segmentation(self):
         print("===Phrasal Segmentation===")        
         #os.chdir(self.root_path)
-        if self.ENABLE_POS_TAGGING == 1:
+        if self.enable_pos_tagging == 1:
             command = self.root_path + "/bin/segphrase_segment --pos_tag" + \
-                        " --thread " + self.THREAD + \
-                        " --model " + self.SEGMENTATION_MODEL + \
+                        " --thread " + self.thread + \
+                        " --model " + self.segmentation_model + \
                         " --text_to_seg_file " + self.tmp + "/tokenized_text_to_seg.txt" + \
                         " --text_seg_pos_tags_file " + self.tmp + "/pos_tags_tokenized_text_to_seg.txt" + \
                         " --output_tokenized_degmented_sentences " + self.tmp + "/tokenized_segmented_sentences.txt" + \
-                        " --highlight-multi " + self.HIGHLIGHT_MULTI + \
-                        " --highlight-single " + self.HIGHLIGHT_SINGLE
+                        " --highlight-multi " + self.highlight_multi + \
+                        " --highlight-single " + self.highlight_single
         else:
             command = self.root_path + "/bin/segphrase_segment" + \
-                        " --thread " + self.THREAD + \
-                        " --model " + self.SEGMENTATION_MODEL + \
+                        " --thread " + self.thread + \
+                        " --model " + self.segmentation_model + \
                         " --text_to_seg_file " + self.tmp + "/tokenized_text_to_seg.txt" + \
                         " --text_seg_pos_tags_file " + self.tmp + "/pos_tags_tokenized_text_to_seg.txt" + \
                         " --output_tokenized_degmented_sentences " + self.tmp + "/tokenized_segmented_sentences.txt" + \
-                        " --highlight-multi " + self.HIGHLIGHT_MULTI + \
-                        " --highlight-single " + self.HIGHLIGHT_SINGLE
+                        " --highlight-multi " + self.highlight_multi + \
+                        " --highlight-single " + self.highlight_single
         os.system(command)
         #os.chdir(self.curent_directory)
 
     def generate_output(self):
         print("===Generating Output===")
-        command = "java " + self.TOKENIZER + " -m segmentation -i " + self.TEXT_TO_SEG
+        command = "java " + self.tokenizer + " -m segmentation -i " + self.text_to_seg
         command = command + " -segmented " + self.tmp + "/tokenized_segmented_sentences.txt"
         command = command + " -o " + self.tmp + "/segmentation.txt"
         command = command + " -tokenized_raw " + self.tmp + "/raw_tokenized_text_to_seg.txt"
         command = command + " -tokenized_id " + self.tmp + "/tokenized_text_to_seg.txt -c N"
         os.system(command)
-        #os.system("java " + self.TOKENIZER + " -m segmentation -i " + self.TEXT_TO_SEG + " -segmented tmp/tokenized_segmented_sentences.txt -o " + self.model + "/segmentation.txt -tokenized_raw tmp/raw_tokenized_text_to_seg.txt -tokenized_id tmp/tokenized_text_to_seg.txt -c N")
-
-# a = Autophrase()
-# a.tokenize("DBLP_full.txt", "EN")
-# #a.LANGUAGE = "EN"
-# a.pos_tagging()
-# a.phrase_segmentation()
-# #a.TEXT_TO_SEG = "data/DBLP_full.txt"
-# a.generate_output()
+        #os.system("java " + self.tokenizer + " -m segmentation -i " + self.text_to_seg + " -segmented tmp/tokenized_segmented_sentences.txt -o " + self.model + "/segmentation.txt -tokenized_raw tmp/raw_tokenized_text_to_seg.txt -tokenized_id tmp/tokenized_text_to_seg.txt -c N")
