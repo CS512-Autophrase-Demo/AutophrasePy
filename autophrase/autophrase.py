d0535a95bdaef36acc419c891b2c8ff4bc2d03a8
Renxuanwang <290484989@qq.com>
2018-05-04 17:09:19 -0500

    Add assert statements

--- autophrase/autophrase.py
+++ autophrase/autophrase.py
@@ -3,10 +3,20 @@ import shutil
 from .pos_tag import pos_tag
 
 class Autophrase:
-    def __init__(self, highlight_multi=0.8, highlight_single=1.0, thread=10, pos_tagging=True):
+    def __init__(self, filename=None, language="EN", pos_tagging=True, highlight_multi=0.8, highlight_single=1.0, thread=10):
+        assert (filename is not None), "file name must be specified!"
+        valid_language = ["EN", "CN", "JA", "ES", "AR", "FR", "IT", "RU"]
+        assert (language in valid_language), "Invalid language! Only support EN, CN, JA, ES, AR, FR, IT, RU!"
+        assert (0 <= highlight_multi and highlight_multi <= 1), "highlight_multi must be between 0 and 1!"
+        assert (0 <= highlight_single and highlight_single <= 1), "highlight_single must be between 0 and 1!"
+        assert (0 <= highlight_single and highlight_single <= 1), "highlight_single must be between 0 and 1!"
+        assert (thread > 0), "thread must be greater than 0!"
+
         self.root_path = os.path.dirname(os.path.abspath(__file__))
         self.curent_directory = os.getcwd()
-        # self.installation_path = os.path.dirname(os.path.abspath(__file__))
+
+        self.text_to_seg = filename
+        self.language = language
         self.highlight_multi = str(highlight_multi)
         self.highlight_single = str(highlight_single)
         self.enable_pos_tagging = pos_tagging
@@ -18,15 +28,14 @@ class Autophrase:
         self.tokenizer = "-cp " + tokenizer_path + " Tokenizer"
     
     @staticmethod
-    def phrasal_segment():
-        a = Autophrase()
-        a.tokenize("DBLP.5K.txt", "EN")
+    def phrasal_segment(filename):
+        a = Autophrase(filename)
+        a.tokenize()
         a.pos_tagging()
         a.phrase_segmentation()
         a.generate_output()
 
-    def tokenize(self, filename, language):
-        self.text_to_seg = filename
+    def tokenize(self):        
         token_mapping = self.model + "/token_mapping.txt"
         if os.path.exists(self.tmp):
             shutil.rmtree(self.tmp)
@@ -35,14 +44,11 @@ class Autophrase:
         tokenized_text_to_seg = self.tmp + "/tokenized_text_to_seg.txt"
         CASE = self.tmp + "/case_tokenized_text_to_seg.txt"
         os.system("java " + self.tokenizer + " -m direct_test -i " + self.text_to_seg + " -o " + tokenized_text_to_seg + " -t " + token_mapping + " -c N -thread " + self.thread)
-        #language_file = open(self.tmp + "/language.txt", 'r')
-        self.language = language
-        print("Detected Language: " + self.language)
 
     def pos_tagging(self):
-        if self.language != "JA"and self.language != "CN" and self.language != "OTHER" and self.enable_pos_tagging == 1:
-            RAW = self.tmp + "/raw_tokenized_text_to_seg.txt"            
-            pos_tag("EN", int(self.thread), RAW, self.tmp)
+        if self.language != "JA" and self.language != "CN" and self.language != "AR" and self.enable_pos_tagging == True:
+            raw = self.tmp + "/raw_tokenized_text_to_seg.txt"            
+            pos_tag(self.language, int(self.thread), raw, self.tmp)
             os.rename(self.tmp + "/pos_tags.txt", self.tmp + "/pos_tags_tokenized_text_to_seg.txt")
 
     def phrase_segmentation(self):
@@ -77,4 +83,3 @@ class Autophrase:
         command = command + " -tokenized_raw " + self.tmp + "/raw_tokenized_text_to_seg.txt"
         command = command + " -tokenized_id " + self.tmp + "/tokenized_text_to_seg.txt -c N"
         os.system(command)
-        #os.system("java " + self.tokenizer + " -m segmentation -i " + self.text_to_seg + " -segmented tmp/tokenized_segmented_sentences.txt -o " + self.model + "/segmentation.txt -tokenized_raw tmp/raw_tokenized_text_to_seg.txt -tokenized_id tmp/tokenized_text_to_seg.txt -c N")
